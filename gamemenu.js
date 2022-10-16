62d03e746d538895b10b5333cb441e8d00d2b855
Aldemir Akpinar <aldemir.akpinar@gmail.com>
2022-10-16 23:20:49 +0300

    Dynamic windows!!

--- gamemenu.js
+++ gamemenu.js
@@ -39,6 +39,8 @@ class gameMenu extends baseScene
         this.load.image('vimicon', 'images/vimlogo.svg');
         this.load.image('clipicon', 'images/clip.png');
         this.load.image('tile', 'images/tile.png');
+        this.load.image('minimize', 'images/minimize.png');
+        this.load.image('close', 'images/close.png');
         // this.load.image('xwindowbox','images/xwindow.png');
 
     }
@@ -75,6 +77,9 @@ class gameMenu extends baseScene
         for (let i=0;i<docks.length;i++) {
             this.createDockButton(i, 0, docks[i]['icon'], docks[i]['text'], this);
         }
+
+        this.createXWindow(500, 200, 200, 100, 'boss mode!');
+        return;
 
         let myWidth = this.game.renderer.width;
         let myHeight = this.game.renderer.height;
@@ -333,10 +338,78 @@ it out.  We did, so it can't be too hard."
            .on('pointerdown', this.dockCalls, this);
     }
 
+
+    createXWindow(x, y, windowWidth, windowHeight, windowText)
+    {
+        // pulled of from windowmaker.org
+        let lightBorder = 0xb6b6b6;
+        let darkBorder = 0x616161;
+        let black = 0x000000;
+        let white = 0xffffff;
+
+        let myContainer = this.add.container(x, y)
+            .setSize(windowWidth, windowHeight)
+            .setDepth(4);
+
+        let myTitle = this.add.graphics()
+            .lineStyle(1, lightBorder, 1)
+            .fillStyle(black);
+
+        myTitle.fillRect(0, 0, windowWidth, 20);
+
+        let myLine = new Phaser.Geom.Line(0, 0, 0, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(0, 0, 20, 0);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(21, 0, 21, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(21, 0, windowWidth - 21, 0);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(windowWidth - 20, 0, windowWidth - 20, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(windowWidth - 20, 0, windowWidth, 0);
+        myTitle.strokeLineShape(myLine);
+
+        myTitle.lineStyle(1, darkBorder, 1);
+        myLine = new Phaser.Geom.Line(0, 20, 20, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(20, 0, 20, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(21, 20, windowWidth - 21, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(windowWidth - 21, 0, windowWidth - 21, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(windowWidth - 21, 20, windowWidth, 20);
+        myTitle.strokeLineShape(myLine);
+        myLine = new Phaser.Geom.Line(windowWidth, 0, windowWidth, 20);
+        myTitle.strokeLineShape(myLine);
+
+        myContainer.add(myTitle);
+
+        let myWindow = this.add.graphics()
+            .lineStyle(1, black, 1)
+            .fillStyle(white);
+
+        myWindow.fillRect(0, 21, windowWidth, windowHeight);
+        myWindow.strokeRect(0, 21, windowWidth, windowHeight);
+        myContainer.add(myWindow);
+
+        let minimize = this.add.image(10,10,'minimize')
+            .setOrigin(0.5);
+        myContainer.add(minimize);
+        let close = this.add.image(windowWidth - 10, 10, 'close')
+            .setOrigin(0.5);
+        myContainer.add(close);
+
+        const textStyle = { fontFamily: "Menlo Regular", fill: "#fff", fontSize: "10px" };
+        let myText = this.add.text(windowWidth / 2, 10, windowText, textStyle)
+            .setOrigin(0.5);
+        myContainer.add(myText);
+
+    }
+
     showPoweredByVim()
     {
         console.log('alo');
     }
 }
-
-
