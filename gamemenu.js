592eef7e0d28a34eae973679b9326ad00f0ee7a0
Aldemir Akpinar <aldemir.akpinar@moya.app>
2025-03-01 21:15:57 +0300

    more typos

--- gamemenu.js
+++ gamemenu.js
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-import baseScene from './baseScene.js';
+import baseScene from './basescene.js';
 import wmaker from './wmaker.js';
 
 class gameMenu extends baseScene
